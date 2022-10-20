d666420d9c7cc4e7ff71b82d2093abf0791af058
akash0651 <kumarakashsahu98765@gmail.com>
2022-10-20 08:20:47 +0530

    Initial comit

---
+++ Admin/function.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+</head>
+
+<body>
+<?php
+function makeconnection()
+{
+	$cn=mysqli_connect("localhost","id19737888_project","c))i!|chEuQLma2j","id19737888_akash");
+	if(mysqli_connect_errno())
+	{
+		echo "failed to connect to mysqli:".mysqli_connect_error();
+	}
+	return $cn;
+}
+
+$cn=mysqli_connect("localhost","root","","travel");
+?>
+</body>
+</html>
